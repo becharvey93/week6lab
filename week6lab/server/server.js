0cb18ce9fb3d7fb6fc3754b00b8bac8c8b15a61c
becharvey93 <becharvey93@users.noreply.github.com>
2018-09-20 16:54:36 +1000

    added server folder and contents

---
+++ week6lab/server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express(); 
+const path = require('path');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const bodyParser = require('body-parser');
+app.use (bodyParser.json()); 
+app.use (bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname , '../dist/chat')));
+require('./routes.js')(app, path); 
+require('./sockets.js')(app, io); 
+require('./listen.js')(http);
+ 
